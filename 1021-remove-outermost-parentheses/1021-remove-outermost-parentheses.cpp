1623ab050dd923525d2ad0919b1916d83006c73f
praveen58522 <115884436+praveen58522@users.noreply.github.com>
2023-02-02 22:31:02 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (80.23%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int c = 0, j=0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                c++;
+            }
+            else{
+                c--;
+            }
+            
+            if(c == 0 && i != 0){
+                s[j]='a';
+                s[i]='a';
+                j = i+1;
+            }
+        }
+        string ans;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!='a'){
+                ans.append(s.substr(i,1));
+            }
+        }
+         return ans;
+    }
+};
