b1db2a78306d0d2deee487f8ec4043600b71649f
praveen58522 <115884436+praveen58522@users.noreply.github.com>
2023-02-03 21:58:39 +0530

    Time: 37 ms (30.75%), Space: 15 MB (49.26%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,7 +4,7 @@ public:
         string ans = "";
         int  j=0;
         for(int i = num.size() - 1; i >= 0; i--){
-            if((int)num[i]%2!=0){
+            if(stoi(num.substr(i, 1))%2!=0){
                 ans = num.substr(0, i+1);
                 return ans;
             }
