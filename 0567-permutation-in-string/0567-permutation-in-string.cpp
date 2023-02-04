88c62854d066ff8b12ea2f2864f46e8d3c65550c
praveen58522 <115884436+praveen58522@users.noreply.github.com>
2023-02-04 18:53:51 +0530

    Time: 15 ms (48.47%), Space: 7.6 MB (27.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         
+        if(s1.size()>s2.size()) return false;
         map<char,int> ms1,ms2;
         
         for(int i = 97; i <= 122; i++)
@@ -12,23 +13,22 @@ public:
         for(int i=0;i<s1.size();i++)
         {
             ms1[s1[i]]++;
+            ms2[s2[i]]++;
         }
-        int start=0,end=0;
+        int start=0,end=s1.size()-1;
         while(end<s2.size())
         {
-           if(end - start + 1 == s1.size()){
-               ms2[s2[end]]++;
-               if(ms1 == ms2) return true;
-               ms2[s2[start]]--;
-               start++;
-               end++;
-           }
-            
-            else{
-                ms2[s2[end]]++;
+            if(ms1==ms2) return true;
+            else
+            {
+                ms2[s2[start]]--;
+                start++;
                 end++;
+                if(end == s2.size()) return false;
+                else
+                ms2[s2[end]]++;
             }
         }
-        return false; 
+        return false;
     }
 };
